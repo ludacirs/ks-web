ff228f70520c2cdec6b19336cf0ceb81084b704a
ludacirs <ludacirs@naver.com>
2020-04-26 02:32:07 +0900

    Create dummy data
    더미 데이터 작성, 컨트롤러 데이터 연결

--- src/main/java/com/you/app/AppApplication.java
+++ src/main/java/com/you/app/AppApplication.java
@@ -1,13 +1,26 @@
 package com.you.app;
 
+import com.you.app.entity.Person;
+import com.you.app.repository.PersonRepository;
+import org.hibernate.dialect.lock.PessimisticEntityLockException;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class AppApplication {
+public class AppApplication implements CommandLineRunner {
+	private final PersonRepository personRepository;
+
+	public AppApplication(PersonRepository personRepository) {//요부분 이해 안됨
+		this.personRepository = personRepository;
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(AppApplication.class, args);
 	}
+	@Override
+	public void run(String... args) throws Exception {
 
+		personRepository.save(new Person("유제호","프론트", "hello@test.com", "010-1234-5678"));
+	}
 }
--- src/main/java/com/you/app/controller/IndexController.java
+++ src/main/java/com/you/app/controller/IndexController.java
@@ -1,13 +1,22 @@
 package com.you.app.controller;
 
+import com.you.app.entity.Person;
+import com.you.app.repository.PersonRepository;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class IndexController {
 
+    private final PersonRepository personRepository;
+    public IndexController(PersonRepository personRepository) {
+        this.personRepository = personRepository;
+    }
     @GetMapping("/")
-    public String index(){
-        return "index";
+    public String index(Model model) {
+        Person person = personRepository.findAll().stream().findFirst().orElse(null);
+        model.addAttribute("person", person);
+        return "resume";
     }
 }
